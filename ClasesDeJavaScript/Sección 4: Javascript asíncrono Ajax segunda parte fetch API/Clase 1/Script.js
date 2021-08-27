1ecb15ebb8a6bd08e532c8fc7146d84c0b741918
maximolicanelo <maximolicanelo@gmail.com>
2021-08-27 11:24:23 -0300

    Crud API fetch Parte 2

--- ClasesDeJavaScript/Sección 4: Javascript asíncrono Ajax segunda parte fetch API/Clase 1/Script.js
+++ ClasesDeJavaScript/Sección 4: Javascript asíncrono Ajax segunda parte fetch API/Clase 1/Script.js
@@ -5,12 +5,19 @@ var url = 'https://bootcamp-dia-3.camilomontoyau.vercel.app/usuarios';
 let ArrayUsuarios = [];
 //tomo el nombre del formulario para agregarlo a la BD
 const NombreUsuario = document.getElementById("nombre");
-
+//tomo el apellido del formulario para agregarlo a la BD
+const ApellidoUsuario = document.getElementById("apellido");
+//tomo el pais del formulario para agregarlo a la BD
+const PaisUsuario = document.getElementById("pais");
 //obtener datos de un servidor remoto
 function Render () 
 {
     //una vez obtenidos los datos , los mostramos en el html
-    const UsuariosRender = ArrayUsuarios.map(usuario => `<tr><td>${usuario.nombre}</td></tr>`).join("");
+    const UsuariosRender = ArrayUsuarios.map(usuario => `<tr>
+                                                             <td>${usuario.nombre ? usuario.nombre : 'vacio'}</td>
+                                                             <td>${usuario.apellido ? usuario.apellido : 'vacio'}</td>
+                                                             <td>${usuario.pais ? usuario.pais : 'vacio'}</td>
+                                                         </tr>`).join("");
     ListaUsuarios.innerHTML = UsuariosRender;
 }
 
@@ -20,7 +27,11 @@ const boton = document.getElementById("boton");
 
 function EnviarDatosUsuario()
 {
-    const datos = {nombre:NombreUsuario.value};
+    const datos = {
+                        nombre:NombreUsuario.value,
+                        apellido:ApellidoUsuario.value,
+                        pais:PaisUsuario.value
+                  };
     
 
     fetch(url, {
@@ -38,6 +49,7 @@ function EnviarDatosUsuario()
         })
     )
 }
+//obtengo los usuarios
 function Refrescar()
 {
     fetch(url)
