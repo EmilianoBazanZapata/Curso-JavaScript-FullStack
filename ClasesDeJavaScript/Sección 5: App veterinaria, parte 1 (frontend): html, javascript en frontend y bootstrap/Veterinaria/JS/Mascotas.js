95239771a5ae7f549d5e0fbded0325db6e0810f3
maximolicanelo <maximolicanelo@gmail.com>
2021-08-30 20:36:27 -0300

    Leer elementos de la lista desde javascript

--- ClasesDeJavaScript/Sección 5: App veterinaria, parte 1 (frontend): html, javascript en frontend y bootstrap/Veterinaria/JS/Mascotas.js
+++ ClasesDeJavaScript/Sección 5: App veterinaria, parte 1 (frontend): html, javascript en frontend y bootstrap/Veterinaria/JS/Mascotas.js
@@ -1,1 +1,25 @@
-console.log("hola");
+const ListaMascotas = document.getElementById('Lista-Mascota');
+let Mascotas = [{
+    tipo: "Gato",
+    nombre: "Simona",
+    propietario: "Esteban"
+}];
+
+function ListarMascotas() {
+    const HtmlMascotas = Mascotas.map((mascota, indice) => `
+        <tr>
+            <td>${indice}</td>
+            <td>${mascota.nombre}</td>
+            <td>${mascota.propietario}</td>
+            <td>${mascota.tipo}</td>
+            <td>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-primary"><i class="fas fa-pencil-alt"></i></button>
+                    <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                </div>
+            </td>
+        </tr>
+    `).join("");
+    ListaMascotas.innerHTML = HtmlMascotas;
+}
+ListarMascotas();
